801129525754720b871583b47119126c5c30372d
Fitz Ignacio <ignacio.fitz@gmail.com>
2023-02-04 22:52:53 -0800

    commit tree 9d627ba944ecee15b25619450be3a7df6bfc5a15

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+const nock = require('nock');
+
+function submitData(name, email) {
+  const data = { name, email };
+
+  nock('http://localhost:3000')
+    .post('/users', data)
+    .reply(201, { id: 123 });
+
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify(data)
+  })
+    .then(response => response.json())
+    .then(json => {
+      const id = json.id;
+      document.body.innerHTML += `<p>New User ID: ${id}</p>`;
+    })
+    .catch(error => {
+      document.body.innerHTML += `<p>Error: ${error.message}</p>`;
+    });
+}
